25101f174bd615993076432e8929121e0a97d5bc
SpiveeWorks <spiveehere@gmail.com>
2018-09-09 14:47:11 +1000

    quad array
    the 1-array workarounds are real

--- main.lua
+++ main.lua
@@ -7,30 +7,28 @@ function tiles.load(self, path)
   self.img_width = self.img:getWidth()
   self.img_height = self.img:getHeight()
 
-end
-
-function tiles.quad(self, x, y)
-  return love.graphics.newQuad(
-    x * self.width, y * self.height,
-    self.width, self.height,
-    self.img_width, self.img_height
-  )
+  tiles.quads = {}
+  for j = 0, 1 do
+    for i = 0, 1 do
+      tiles.quads[j * 2 + i + 1] = love.graphics.newQuad(
+        i * self.width, j * self.height,
+        self.width, self.height,
+        self.img_width, self.img_height
+      )
+    end
+  end
 end
 
 
 function love.load()
   tiles:load('tileset.png')
-
-  BlockQuad = tiles:quad(0, 0)
-  EmptyQuad = tiles:quad(1, 1)
-
 end
 
 function love.draw()
   for i = 1,3 do
     for j = 1,3 do
-      love.graphics.draw(tiles.img, BlockQuad, (10 + i) * 32, (8 + j) * 32)
+      love.graphics.draw(tiles.img, tiles.quads[1], (10 + i) * 32, (8 + j) * 32)
     end
   end
-  love.graphics.draw(tiles.img, EmptyQuad, 12 * 32, 10 * 32)
+  love.graphics.draw(tiles.img, tiles.quads[4], 12 * 32, 10 * 32)
 end
