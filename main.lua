9837c6817de1348d7421c55e8ab13c708e267af2
SpiveeWorks <spiveehere@gmail.com>
2018-09-09 16:49:26 +1000

    controls!

--- main.lua
+++ main.lua
@@ -46,22 +46,44 @@ function love.load()
 end
 
 local modes = {}
-local floor = 16*32
+local FLOOR_HEIGHT = 16*32
+local WALK_SPEED = 10
+local JUMP_HEIGHT = -100
+local JUMP_TIME = 15
+
+-- s(t) = 1/2 at^2 + ut
+-- s(2*_t) = 0 => a = -u/_t
+--             => s = ut(1 - t/(2*_t))
+-- s(_t) = _s  => u = 2*_s/_t
+local JUMP_SPEED = 2 * JUMP_HEIGHT / JUMP_TIME
+local JUMP_ACC = - JUMP_SPEED / JUMP_TIME
 
 function modes.update(body)
   modes[body.mode](body)
 end
 
 function modes.falling(body)
-  if body.y > floor then
+  if body.y > FLOOR_HEIGHT then
     body.mode = "walking"
-    body.y = floor
+    body.y = FLOOR_HEIGHT
     body.vel_y = 0
     body.acc_y = 0
   end
 end
 
 function modes.walking(body)
+  body.vel_x = 0
+  if love.keyboard.isDown("left") then
+    body.vel_x = -WALK_SPEED
+  end
+  if love.keyboard.isDown("right") then
+    body.vel_x = body.vel_x + WALK_SPEED
+  end
+  if love.keyboard.isDown("space") then
+    body.mode = "falling"
+    body.vel_y = JUMP_SPEED
+    body.acc_y = JUMP_ACC
+  end
 end
 
 function love.update()
