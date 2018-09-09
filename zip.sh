144cb00a9f14b9d9a96d5ed03731b38320a072b0
SpiveeWorks <spiveehere@gmail.com>
2018-09-09 17:29:54 +1000

    make game directory
    all the source files and resources are here,
    no need to touch zip.sh now

--- zip.sh
+++ zip.sh
@@ -1,2 +1,10 @@
 #/bin/sh
-7z a love/tj.love *.lua *.png
+if [ -d love ]; then
+    if [ -f love/tj.love ]; then
+        rm love/tj.love
+    fi
+else
+    mkdir love
+fi
+
+zip -9 -r love/tj.love game/*
